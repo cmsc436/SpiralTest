5d5ff6d24765e8ebe8a888572559c53f9bf95dd5
Samantha Koy <skoy@terpmail.umd.edu>
2017-04-25 00:03:18 -0400

    Copy over DrawingView

---
+++ app/src/main/java/cmsc436/umd/edu/spiraltest/DrawingView.java
@@ -0,0 +1,85 @@
+package cmsc436.umd.edu.spiraltest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View {
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+    private boolean pause;
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context,attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(0xFFFF0000);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(40);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        //i know, switch cases lmao
+        if (!pause) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    drawPath.moveTo(touchX, touchY);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    drawPath.lineTo(touchX, touchY);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    drawCanvas.drawPath(drawPath, drawPaint);
+                    drawPath.reset();
+                    break;
+                default:
+                    return false;
+            }
+
+            invalidate();
+        }
+
+        return true;
+    }
+
+    // for when the timer goes up on the spiral test
+    public void pause() {
+        pause = true;
+    }
+
+    public void clear() {
+        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+    }
+}
+
