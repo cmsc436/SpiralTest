da68d7b6b3e8ce122dbe2b7b0a25bc0d5f891b13
bbwong23 <bethanywong1216@gmail.com>
2017-05-07 16:03:29 -0400

    set up layout for menu

---
+++ app/src/main/java/cmsc436/umd/edu/spiraltest/PracticeMenuFragment.java
@@ -0,0 +1,29 @@
+package cmsc436.umd.edu.spiraltest;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PracticeMenuFragment extends Fragment {
+
+
+    public PracticeMenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_practice_menu, container, false);
+    }
+
+}
